f0a9fc77f2402d239a1b05093b71feb3f2b13c79
Gabin Durand <gabindx1@gmail.com>
2023-11-16 09:59:57 +0100

    json to kotlin

--- app/src/main/java/com/example/cvdurandgabin/models/Models.kt
+++ app/src/main/java/com/example/cvdurandgabin/models/Models.kt
@@ -13,30 +13,65 @@ class TmdbMovie(
     val genre_ids: List<Int> = listOf(),
     val poster_path: String? = "")
 
-class TmdbSerieResult(
-    var page: Int = 0,
-    val results: List<TmdbSerie> = listOf())
+data class TmdbSerieResult(
+    val page: Int,
+    val results: List<TmdbSerie> = listOf()
+)
 
-class TmdbSerie(
-    var overview: String = "",
-    val release_date: String = "",
-    val id: String = "",
-    val name: String = "",
-    val original_name: String = "",
-    val backdrop_path: String? = "",
-    val genre_ids: List<Int> = listOf(),
-    val poster_path: String? = "")
+data class TmdbSerie(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val first_air_date: String,
+    val genre_ids: List<Int>,
+    val id: Int,
+    val media_type: String,
+    val name: String,
+    val origin_country: List<String>,
+    val original_language: String,
+    val original_name: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val vote_average: Double,
+    val vote_count: Int
+)
 
-class TmdbActorResult(
-    var page: Int = 0,
-    val results: List<TmdbActor> = listOf())
 
-class TmdbActor(
-    var overview: String = "",
-    val release_date: String = "",
-    val id: String = "",
-    val name: String = "",
-    val original_name: String = "",
-    val backdrop_path: String? = "",
-    val genre_ids: List<Int> = listOf(),
-    val profile_path: String? = "")
+data class TmdbActorResult(
+    val page: Int,
+    val results: List<TmdbActor> = listOf()
+)
+
+data class TmdbActor(
+    val adult: Boolean,
+    val gender: Int,
+    val id: Int,
+    val known_for: List<KnownFor>,
+    val known_for_department: String,
+    val name: String,
+    val original_name: String,
+    val popularity: Double,
+    val profile_path: String
+)
+
+data class KnownFor(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val first_air_date: String,
+    val genre_ids: List<Int>,
+    val id: Int,
+    val media_type: String,
+    val name: String,
+    val origin_country: List<String>,
+    val original_language: String,
+    val original_name: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+)
